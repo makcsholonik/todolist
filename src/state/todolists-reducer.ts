1581b9094bd7b186b5b5796736c6eeeda6f7d66e
makcsholonik <makcsholonik@gmail.com>
2021-03-24 18:58:28 +0300

    08 (reducers + tests + tdd) - 12 tasks

---
+++ src/state/todolists-reducer.ts
@@ -0,0 +1,35 @@
+import { TodolistType } from "../App";
+import { v1 } from "uuid";
+
+type ActionType = {
+	type : string
+	[ key : string ] : any
+}
+
+export const todolistsReducer = ( state : Array<TodolistType>, action : ActionType ) : Array<TodolistType> => {
+	switch (action.type) {
+		case 'REMOVE-TODOLIST':
+			return state.filter ( tl => tl.id !== action.id );
+		case 'ADD-TODOLIST':
+			return [...state, {
+				id:v1 (),
+				title:action.title,
+				filter:'all'
+			}];
+		case 'CHANGE-TODOLIST-TITLE':
+			const todolist = state.find ( tl => tl.id === action.id );
+			if (todolist) {
+				todolist.title = action.title;
+			}
+			return [...state];
+		case 'CHANGE-TODOLIST-FILTER':
+			let todolistFilter = state.find ( tl => tl.id === action.id );
+			if (todolistFilter) {
+				todolistFilter.filter = action.filter;
+			}
+			return [...state];
+		default:
+			throw new Error('I dont understand this type')
+
+	}
+}
