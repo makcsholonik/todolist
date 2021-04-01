e255e123b3e6cef886018bee95a5bcf6b9b681b0
makcsholonik <makcsholonik@gmail.com>
2021-04-01 06:24:24 +0300

    09 (reducers + tests + tdd - part 2) - add task

--- src/state/tasks-reducer.ts
+++ src/state/tasks-reducer.ts
@@ -1,39 +1,48 @@
 import { TaskStateType } from "../App";
+import { v1 } from "uuid";
 
 type RemoveTaskActionType = {
 	type : 'REMOVE-TASK'
-	id: string
-	todolistId: string
+	id : string
+	todolistId : string
 }
-type SomeActionCreator2ActionType = {
-	type : ''
+type AddTaskActionType = {
+	type : 'ADD-TASK'
+	title : string
+	todolistId : string
 }
 
-export type ActionType = RemoveTaskActionType | SomeActionCreator2ActionType
+export type ActionType = RemoveTaskActionType | AddTaskActionType
 
 
 export const tasksReducer = ( state : TaskStateType, action : ActionType ) : TaskStateType => {
 	switch (action.type) {
-		case 'REMOVE-TASK':
+		case 'REMOVE-TASK': {
 			// делаем копию state, далее ее будем изменять и возвращать
-			const stateCopy = {...state};
+			const stateCopy = { ...state };
 			// достаём массив тасок из копии стейта по ID тудулиста который приходит к нам из action
-			const tasks = state[action.todolistId];
+			const tasks = state[ action.todolistId ];
 			// получаем отфильтрованные таски
-			const filteredTasks = tasks.filter(task => task.id !== action.id);
+			const filteredTasks = tasks.filter ( task => task.id !== action.id );
 			// записываем отфильтрованные таски в stateCopy
-			stateCopy[action.todolistId] = filteredTasks;
+			stateCopy[ action.todolistId ] = filteredTasks;
 			return stateCopy
-		case '':
-			return state
+		}
+		case 'ADD-TASK': {
+			const stateCopy = { ...state };
+			const newTask = { id : v1 (), title : action.title, isDone : false };
+			const tasks = state[ action.todolistId ];
+			stateCopy[ action.todolistId ] = [newTask, ...tasks];
+			return stateCopy
+		}
 		default:
 			throw new Error ( 'I dont understand this type' )
 	}
 }
 
-export const removeTaskAC = (id: string, todolistId: string) : RemoveTaskActionType => {
+export const removeTaskAC = ( id : string, todolistId : string ) : RemoveTaskActionType => {
 	return { type : 'REMOVE-TASK', id, todolistId }
 }
-export const Some2AC = () : SomeActionCreator2ActionType => {
-	return { type : '' }
+export const addTaskAC = ( title : string, todolistId : string ) : AddTaskActionType => {
+	return { type : 'ADD-TASK', title, todolistId }
 }
